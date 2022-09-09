a520d2a9d568271c4c228c3a143759395d24ecde
Adurzada <93799478+Adurzada@users.noreply.github.com>
2022-09-09 02:58:31 -0400

    Update sendinginfo.js

--- sendinginfo.js
+++ sendinginfo.js
@@ -7,8 +7,7 @@ const res = document.getElementById('results-1')
 
 
 const sendform = (em, mes) => {
-  let myForm = document.getElementById("myform1");
-  let formData = new FormData(myForm);
+  let formData = new FormData(document.getElementById("myform1"));
   formData.append("email",em);
   formData.append("query",mes);
   var val = '\n' + em + ' -> ' + mes;
